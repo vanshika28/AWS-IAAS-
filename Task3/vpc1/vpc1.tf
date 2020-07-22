0fc27c41bc4946afdb89671721fc6fd356ba7e5b
vanshika28 <vanshikanonu1999@gmail.com>
2020-07-23 00:43:56 +0530

    vpc

--- Task3/vpc1/vpc1.tf
+++ Task3/vpc1/vpc1.tf
@@ -78,6 +78,14 @@ ingress {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+ ingress {
+    description = "TLS from VPC"
+    from_port   = 0 
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 
   egress {
     from_port   = 0
@@ -147,4 +155,3 @@ tags = {
    Name = "sql"
   }
 }
-
